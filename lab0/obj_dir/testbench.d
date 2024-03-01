dd09952866e19925870b6f3e12c2d1b5ba40c56f
Jimmy Wu <107825203+jimmy93029@users.noreply.github.com>
2024-03-01 15:13:22 +0800

    Delete lab0/obj_dir directory

--- lab0/obj_dir/testbench.d
+++
@@ -1,6 +0,0 @@
-testbench.o: ../part2/testbench.cpp \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Valu.h
