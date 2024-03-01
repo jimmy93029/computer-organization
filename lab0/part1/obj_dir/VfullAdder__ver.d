a12a2bb1570e735acd6c272a43cc0ea9cdea7f70
jimmywu <jimmywu0229@gmail.com>
2024-03-01 15:38:32 +0800

    Commit message

--- lab0/part1/obj_dir/VfullAdder__ver.d
+++
@@ -1,1 +0,0 @@
-obj_dir/VfullAdder.cpp obj_dir/VfullAdder.h obj_dir/VfullAdder.mk obj_dir/VfullAdder__Slow.cpp obj_dir/VfullAdder__Syms.cpp obj_dir/VfullAdder__Syms.h obj_dir/VfullAdder__ver.d obj_dir/VfullAdder_classes.mk  : /usr/bin/verilator_bin /usr/bin/verilator_bin fullAdder.v 
