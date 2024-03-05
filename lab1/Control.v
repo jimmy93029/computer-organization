8ba9cec8b248995af7c911c1fc2c2757ffd45a6d
jimmywu <jimmywu0229@gmail.com>
2024-03-05 22:24:43 +0800

    add main  control

--- lab1/Control.v
+++ lab1/Control.v
@@ -11,7 +11,22 @@ module Control (
 
     // TODO: implement your Control here
     // Hint: follow the Architecture (figure in spec) to set output signal
-    
+    assign {ALUSrc, memtoReg, regWrite, memRead, memWrite, branch, ALUOp} = control;
+
+    always @(*) begin
+        case(opcode)
+            7'0110011:  
+                control <= 8'b00100010;  // R type
+            7'0010011:
+                control <= 8'b11110000;  // I type
+            7'0100011: 
+                control <= 8'b1x001000;  // S type
+            7'1100011:
+                control <= 8'b0x000101;  // B type
+            default:
+                control <= 8'bxxxxxxxx;
+        endcase
+    end
 
 endmodule
 
