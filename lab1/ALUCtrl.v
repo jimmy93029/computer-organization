521e61269e1990d751dac014b04a5b91f714ed9b
jimmywu <jimmywu0229@gmail.com>
2024-03-15 15:46:07 +0800

    add makefile

--- lab1/ALUCtrl.v
+++ lab1/ALUCtrl.v
@@ -22,13 +22,13 @@ module ALUCtrl (
             2'b1x:
                 case(funct7)
                     7'd0:
-                        case(funct3):
+                        case(funct3)
                             3'b000: 
                                 ALUCtl <= 4'b0010; 
                             3'b111:
                                 ALUCtl <= 4'b0000;
                             3'b110:
-                                ALUCtl <= 4'b'0001;
+                                ALUCtl <= 4'b0001;
                         endcase
                     7'd32:
                         ALUCtl <= 4'b0110;
