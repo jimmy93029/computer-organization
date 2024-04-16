651eff32c12819c46cd3d6929420c37c51f204cd
jimmy93029 <jimmywu0229@gmail.com>
2024-04-16 16:16:04 +0800

    wrong opcode in ALUCtrl

--- lab1and2/ALU_Branch_Ctrl.v
+++ lab1and2/ALU_Branch_Ctrl.v
@@ -13,33 +13,11 @@ module ALU_Branch_Ctrl (
 always @*
 begin
     case(ALUOp)
+
         // case J type, I type (1), I type (2), S type, B type
         2'b00:
-            ALUCtl = 4'b0010;  // jal, jalr, lw, sw, beq, bnq, blt, bge : add
-        // case R type
-        2'b10:
-            case(funct7)
-                1'b0:
-                    case(funct3)
-                        3'b000: 
-                            ALUCtl = 4'b0010;  // add
-                        3'b110:
-                            ALUCtl = 4'b0111;  // slt : A < B
-                        3'b010:
-                            ALUCtl = 4'b0001;  // or
-                        3'b111:
-                            ALUCtl = 4'b0000;  // and
-                        default:
-                            ALUCtl = 4'bxxxx;
-                    endcase
-                1'b1:
-                    case(funct3)
-                        3'b000:
-                            ALUCtl = 4'b0110;  // sub
-                        default:
-                            ALUCtl = 4'bxxxx;
-                    endcase
-            endcase
+            ALUCtl = 4'b0010;          // jal, jalr, lw, sw, beq, bnq, blt, bge : add
+            
         // case I type (3)
         2'b11:
             case(funct3)
@@ -54,6 +32,31 @@ begin
                 default:
                     ALUCtl = 4'bxxxx;
             endcase
+
+        // case R type
+        2'b10:
+            case(funct7)
+                1'b0:
+                    case(funct3)
+                        3'b000: 
+                            ALUCtl = 4'b0010;  // add
+                        3'b010:
+                            ALUCtl = 4'b0111;  // slt : A < B
+                        3'b110:
+                            ALUCtl = 4'b0001;  // or
+                        3'b111:
+                            ALUCtl = 4'b0000;  // and
+                        default:
+                            ALUCtl = 4'bxxxx;
+                    endcase
+                1'b1:
+                    case(funct3)
+                        3'b000:
+                            ALUCtl = 4'b0110;  // sub
+                        default:
+                            ALUCtl = 4'bxxxx;
+                    endcase
+            endcase
         default: ALUCtl = 4'bxxxx;  // Unsupported
     endcase
 end
@@ -67,7 +70,7 @@ always @* begin
         3'b000:
             BranchCtl = 4'b1100;  // beq : A == B
         3'b001:
-            BranchCtl = 4'b1101;  // bnq : A != B
+            BranchCtl = 4'b1101;  // bne : A != B
         3'b100:
             BranchCtl = 4'b0111;  // blt : A < B
         3'b101:
