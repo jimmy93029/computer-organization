09ef1669d73f2f288a8510747fc3fee0faa2cf33
jimmy93029 <jimmywu0229@gmail.com>
2024-05-15 23:02:37 +0800

    finish adding forwarding

--- lab3andlab4/PipelineCPU.v
+++ lab3andlab4/PipelineCPU.v
@@ -14,9 +14,9 @@ module PipelineCPU (
 
 
 // PC related wires
-wire [31:0] IF_pc, ID_pc, Ex_pc;  
+wire [31:0] IF_pc, ID_pc, Ex_pc, Mem_pc, Wb_pc;  
 wire signed [31:0] IF_pc_mux;
-wire signed [31:0] pc_add4;
+wire signed [31:0] IF_pc_add4, Wb_pc_add4;
 wire [31:0] IF_instruction, ID_instruction;
 wire [3:0] ID_BranchCtl, Ex_BranchCtl;
 wire Ex_BranchOut;
@@ -29,22 +29,21 @@ wire ID_regWrite, Ex_regWrite, Mem_regWrite, Wb_regWrite;
 wire ID_memRead, Ex_memRead, Mem_memRead;
 wire ID_memWrite, Ex_memWrite, Mem_memWrite;
 wire ID_branch, Ex_branch;
-wire ID_jump, Ex_jump;     
+wire ID_jump, Ex_jump, Mem_jump, Wb_jump;     
 wire ID_pcUse, Ex_pcUse; 
-wire[1:0] ID_ALUOp, Ex_ALUOp;
+wire[1:0] ID_ALUOp;
 
 
 // register data wires, Memory 
-wire signed [31:0] Wb_writeData_mux1;
-wire signed [31:0] Wb_writeData_mux2;
+wire signed [31:0] Wb_writeData;
 wire[31:0] ID_readData1, Ex_readData1;
-wire[31:0] ID_readData2, Ex_readData2;
+wire[31:0] ID_readData2, Ex_readData2, Mem_readData2;
 wire[31:0] Mem_readData3, Wb_readData3;
 
 
 // register address wires
-input [5:0] Ex_RegRs, Mem_RegRs;
-input [5:0] Ex_RegRt;
+input [5:0] Ex_RegRs1, Mem_RegRs1;
+input [5:0] Ex_RegRs2;
 input [5:0] Ex_RegRd, Mem_RegRd, Wb_RegRd;
 
 
@@ -52,7 +51,7 @@ input [5:0] Ex_RegRd, Mem_RegRd, Wb_RegRd;
 wire signed [31:0] ID_imm, Ex_imm; 
 wire signed [31:0] Ex_aluSrc1; 
 wire signed [31:0] Ex_aluSrc2;
-wire signed [31:0] Ex_ALUOut, Mem_ALUOut;
+wire signed [31:0] Ex_ALUOut, Mem_ALUOut, Wb_ALUOut;
 wire[3:0] ID_ALUCtl, Ex_ALUCtl;
 
 
@@ -73,7 +72,7 @@ PC m_PC(
 
 Mux2to1 #(.size(32)) m_Mux_PC(
     .sel((Ex_branch && Ex_BranchOut) || Ex_jump),
-    .s0(pc_add4),   
+    .s0(IF_pc_add4),   
     .s1(Ex_ALUOut),
     .out(IF_pc_mux)
 );
@@ -82,7 +81,7 @@ Mux2to1 #(.size(32)) m_Mux_PC(
 Adder m_Adder_1(
     .a(IF_pc),
     .b(32'd4),
-    .sum(pc_add4)
+    .sum(IF_pc_add4)
 );
 
 
@@ -124,7 +123,7 @@ Register m_Register(
     .readReg1(ID_instruction[19:15]),
     .readReg2(ID_instruction[24:20]),
     .writeReg(ID_instruction[11:7]),
-    .writeData(Wb_writeData_mux2),
+    .writeData(Wb_writeData),
     .readData1(ID_readData1),
     .readData2(ID_readData2)
 );
@@ -145,14 +144,14 @@ ImmGen m_ImmGen(
 );
 
 
-Pipe_reg m_IF_ID(
+Pipe_reg m_ID_Ex(
     .clk(clk),
     .rst(start),
     .flush(),               //-------------------- unfill in -------------------------//
-    .data_i({ID_immUse, ID_memtoReg, ID_regWrite, ID_memRead, ID_memWrite, ID_branch, ID_jump, ID_pcUse, ID_ALUOp, ID_pc, ID_imm, 
+    .data_i({ID_immUse, ID_memtoReg, ID_regWrite, ID_memRead, ID_memWrite, ID_branch, ID_jump, ID_pcUse, ID_pc, ID_imm, 
                 ID_readData1, ID_readData2, ID_ALUCtl, ID_BranchCtl, ID_instruction[19:15], ID_instruction[24:20], ID_instruction[11:7]}),
-    .data_o({Ex_immUse, Ex_memtoReg, Ex_regWrite, Ex_memRead, Ex_memWrite, Ex_branch, Ex_jump, Ex_pcUse, Ex_ALUOp, Ex_pc, Ex_imm, 
-                Ex_readData1, Ex_readData2, Ex_ALUCtl, Ex_BranchCtl, Ex_RegRt, Ex_RegRs, Ex_RegRd})
+    .data_o({Ex_immUse, Ex_memtoReg, Ex_regWrite, Ex_memRead, Ex_memWrite, Ex_branch, Ex_jump, Ex_pcUse, Ex_pc, Ex_imm, 
+                Ex_readData1, Ex_readData2, Ex_ALUCtl, Ex_BranchCtl, Ex_RegRs1, Ex_RegRs2, Ex_RegRd})
 )
 
 
@@ -160,8 +159,8 @@ Pipe_reg m_IF_ID(
 
 ALU m_ALU(
     .ALUCtl(Ex_ALUCtl),
-    .A(Ex_aluSrc1_mux2),
-    .B(Ex_aluSrc2_mux2),
+    .A(Ex_aluSrc1),
+    .B(Ex_aluSrc2),
     .ALUOut(Ex_ALUOut)
 );
 
@@ -185,15 +184,15 @@ Mux2to1 #(.size(32)) m_Mux1_ALUSrc1(
 
 
 Mux3to1 #(.size(32)) m_Mux2_ALUSrc1(
-    .sel(),
-    .s0(out2),
-    .s1(),
-    .s2(),
+    .sel(forwardA),
+    .s0(out1),
+    .s1(Wb_writeData),
+    .s2(Mem_ALUOut),
     .out(Ex_aluSrc1)
 );
 
 
-Mux2to1 #(.size(32)) m_Mux2_ALU(
+Mux2to1 #(.size(32)) m_Mux1_ALUSrc2(
     .sel(Ex_immUse),
     .s0(Ex_readData2),
     .s1(Ex_imm),
@@ -201,11 +200,11 @@ Mux2to1 #(.size(32)) m_Mux2_ALU(
 );
 
 
-Mux3to1 #(.size(32)) m_Mux2_ALUSrc1(
-    .sel(),
+Mux3to1 #(.size(32)) m_Mux2_ALUSrc2(
+    .sel(forwardB),
     .s0(out2),
-    .s1(),
-    .s2(),
+    .s1(Wb_writeData),
+    .s2(Mem_ALUOut),
     .out(Ex_aluSrc2)
 );
 
@@ -213,6 +212,23 @@ Mux3to1 #(.size(32)) m_Mux2_ALUSrc1(
 ForwardUnit m_ForwardUnit(
     .Mem_regWrite(Mem_regWrite),
     .Wb_regWrite(Wb_regWrite),
+    .Mem_RegRd(Mem_RegRd),
+    .Wb_RegRd(Wb_RegRd),
+    .Ex_RegRs(Ex_RegRs),
+    .Ex_RegRt(Ex_RegRt),
+    .forwardA(forwardA),
+    .forwardB(forwardB)
+)
+
+
+Pipe_reg m_Ex_Mem(
+    .clk(clk),
+    .rst(start),
+    .flush(1'd0)
+    .data_i({Ex_memWrite, Ex_memRead, Ex_jump, 
+                Ex_pc, Ex_RegRd, Ex_ALUOut, Ex_readData2}),
+    .data_o({Mem_memWrite, Mem_memRead, Mem_jump, 
+                Mem_pc, Mem_RegRd,  Mem_ALUOut, Mem_readData2})
 )
 
 
@@ -221,27 +237,50 @@ ForwardUnit m_ForwardUnit(
 DataMemory m_DataMemory(
     .rst(start),
     .clk(clk),
-    .memWrite(memWrite),
-    .memRead(memRead),
-    .address(ALUOut),
-    .writeData(readData2),
-    .readData(readData3)
+    .memWrite(Mem_memWrite),
+    .memRead(Mem_memRead),
+    .address(Mem_ALUOut),
+    .writeData(Mem_readData2),
+    .readData(Mem_readData3)
 );
+
+
+Pipe_reg m_Ex_Mem(
+    .clk(clk),
+    .rst(start),
+    .flush(1'd0)
+    .data_i({Mem_memtoReg, Mem_regWrite, Mem_jump, 
+                Mem_pc, Mem_ALUOut, Mem_readData3, Mem_RegRd}),
+    .data_o({Wb_memtoReg, Wb_regWrite, Wb_jump,
+                Wb_pc, Wb_ALUOut, Wb_readData3, Wb_RegRd})
+)
+
 
 // stage 5 : Wb (write back) -------------------------------------------------------
 
+
+wire signed [31:0] out3;
+
 Mux2to1 #(.size(32)) m_Mux1_WriteData(
-    .sel(memtoReg),
-    .s0(ALUOut),
-    .s1(readData3),
-    .out(WriteData_mux1_out)
+    .sel(Wb_memtoReg),
+    .s0(Wb_ALUOut),
+    .s1(Wb_readData3),
+    .out(out3)
 );
 
+
 Mux2to1 #(.size(32)) m_Mux2_WriteData(
-    .sel(jump),
-    .s0(WriteData_mux1_out),
-    .s1(pc_add4),
-    .out(WriteData_mux2_out)
+    .sel(Wb_jump),
+    .s0(out3),
+    .s1(Wb_pc_add4),
+    .out(Wb_writeData)
+);
+
+
+Adder m_Adder_2(
+    .a(Wb_pc),
+    .b(32'd4),
+    .sum(Wb_pc_add4)
 );
 
 
